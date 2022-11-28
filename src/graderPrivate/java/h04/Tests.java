9d829ad8bd09da7728c9e53b7435889a3612726d
Dustin Glaser <dustin.glaser@posteo.de>
2022-11-28 19:28:04 +0100

    use custom string matcher

--- src/graderPrivate/java/h04/Tests.java
+++ src/graderPrivate/java/h04/Tests.java
@@ -1,16 +1,51 @@
 package h04;
 
+import org.tudalgo.algoutils.reflect.TestUtils;
 import org.tudalgo.algoutils.tutor.general.match.BasicStringMatchers;
+import org.tudalgo.algoutils.tutor.general.match.Match;
 import org.tudalgo.algoutils.tutor.general.match.Matcher;
 import org.tudalgo.algoutils.tutor.general.match.MatcherFactories.StringMatcherFactory;
 import org.tudalgo.algoutils.tutor.general.match.Stringifiable;
 
 public class Tests {
 
+    private static final double MINIMUM_SIMILARITY = .8;
+
     private static final StringMatcherFactory STRING_MATCHER_FACTORY = BasicStringMatchers::identical;
 
     public static <T extends Stringifiable> Matcher<T> stringMatcher(String string) {
-        return STRING_MATCHER_FACTORY.matcher(string);
+
+        return new Matcher<T>() {
+
+            @Override
+            public <ST extends T> Match<ST> match(ST object) {
+                return new Match<>() {
+
+                    final double similarity = TestUtils.similarity(object.string(), string);
+
+                    @Override
+                    public boolean matched() {
+                        return similarity >= MINIMUM_SIMILARITY;
+                    }
+
+                    @Override
+                    public ST object() {
+                        return object;
+                    }
+
+                    @Override
+                    public int compareTo(Match<ST> other) {
+                        if (!other.matched()) {
+                            return matched() ? 1 : 0;
+                        } else if (!matched()) {
+                            return -1;
+                        }
+                        double otherSimilarity = TestUtils.similarity(string, other.object().string());
+                        return Double.compare(similarity, otherSimilarity);
+                    }
+                };
+            }
+        };
     }
 }
 
